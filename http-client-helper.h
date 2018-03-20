28f4e156bbe7adc9ad3f07b7a6989afa7c32d240
alexander-ray <1alexray@gmail.com>
2018-03-20 15:58:30 -0600

    More documentation

--- http-client-helper.h
+++ http-client-helper.h
@@ -3,10 +3,45 @@
 
 #include "http-client.h"
 
+/*
+    REQUIRES:
+        Valid (initialized) pointer to curl struct
+        port number of server
+
+    MODIFIES:
+        curl struct pointed to by argument
+
+    RETURNS:
+        0 on success
+*/
 int set_global_opts(CURL *curl, long port);
-int info_callback(void *p, curl_off_t dltotal, curl_off_t dlnow,
-                  curl_off_t ultotal, curl_off_t ulnow);
-int create_full_path(char *remote_path);
+
+/*
+    REQUIRES:
+        Valid (initialized) pointer to curl struct
+        local_fn, remote_path
+
+    MODIFIES:
+        curr_fd, curl struct
+
+    RETURNS:
+        0 on success
+        -1 on bad file open, fstat call
+*/
 int init_file_upload(CURL *curl, char *local_fn, char *remote_path);
+
+/*
+    REQUIRES:
+        successful completion of curl_init (for remote base url)
+        remote path of destination on server
+
+    MODIFIES:
+        full_remote_path
+
+    RETURNS:
+        0 on success
+        -1 for bad realloc
+*/
+int create_full_path(char *remote_path);
 
 #endif
